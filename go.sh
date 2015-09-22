4c227c083e2effd2f37935c660382af172cc7b80
ISHIDA Akio <ishida@coderium.com>
2015-09-22 09:30:14 +0900

    WIP

--- go.sh
+++ go.sh
@@ -2,11 +2,15 @@
 [ -d www  ] || mkdir www
 [ -d logs ] || mkdir logs
 [ -d tmp  ] || mkdir tmp
-HTTPD=/usr/sbin/httpd
+HTTPD=/usr/sbin/apache2
 PORT=8000
+SERVER_ROOT=/etc/apache2
+MODULES=/usr/lib/apache2/modules
 cat <<EOD > httpd.conf
-ServerRoot   /etc/httpd
-LockFile     $PWD/logs/httpd.lock
+ServerRoot   $SERVER_ROOT
+#LockFile     $PWD/logs/httpd.lock
+User         $USER
+Group        $USER
 Listen       $PORT
 DocumentRoot $PWD/www/
 PidFile      $PWD/logs/httpd.pid
@@ -16,13 +20,16 @@ TypesConfig  /etc/mime.types
 # --------------------------------------------------------
 # Modules
 # --------------------------------------------------------
-LoadModule mime_module modules/mod_mime.so
+LoadModule mpm_prefork_module $MODULES/mod_mpm_prefork.so
+LoadModule authn_core_module  $MODULES/mod_authn_core.so
+LoadModule authz_core_module $MODULES/mod_authz_core.so
+LoadModule mime_module $MODULES/mod_mime.so
 AddDefaultCharset UTF-8
 
 # --------------------------------------------------------
 # PHP
 # --------------------------------------------------------
-LoadModule  php5_module modules/libphp5.so
+LoadModule  php5_module $MODULES/libphp5.so
 AddType     application/x-httpd-php .php
 php_value   session.save_path $PWD/tmp
 EOD
@@ -31,8 +38,5 @@ $HTTPD -f $PWD/httpd.conf
 
 # pause trick
 echo "httpd startd on $PORT."
-echo "Hit Ctrl-D to stop httpd."
-trap "" 2
-cat > /dev/null
-$HTTPD -f $PWD/httpd.conf -k stop
-echo "stop"
+trap "$HTTPD -f $PWD/httpd.conf -k stop" INT
+tail -f logs/error.log
